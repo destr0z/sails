7ecfc57c4c874ad121c1377405b348c794bed856
Daniel Karlsson <daniel.karlsson@frojd.se>
2013-12-23 16:23:56 +0100

    Updated stores

--- Assets/Scripts/Gui/ShopMenu.cs
+++ Assets/Scripts/Gui/ShopMenu.cs
@@ -5,23 +5,35 @@ public class ShopMenu : MonoBehaviour {
 
     public string item;
     public int amount;
+	public string storename;
 
     bool showGui = false;
+
+	void Start() 
+	{
+		if(!Stores.StoreList.ContainsKey(storename)) {
+			Stores.StoreList.Add(storename, new Stores.StoreInventory() {StoreItem = item, StoreItemAmount=amount});
+		}
+	}
 
     void OnGUI()
     {
         if (showGui)
         {
             GUI.Box(new Rect(0, 0, 80, 100), "Shop");
-            GUI.Label(new Rect(0, 20, 80, 20), item + ": "+amount);
+			GUI.Label(new Rect(0, 20, 80, 20), item + ": "+Stores.StoreList[storename].StoreItemAmount);
             if (GUI.Button(new Rect(0, 40, 80, 20), "Buy"))
             {
-                amount--;
+				if(Stores.StoreList[storename].StoreItemAmount > 0) {
+					Stores.StoreList[storename].StoreItemAmount--;
+				}
             }
 
             if (GUI.Button(new Rect(0, 60, 80, 20), "Sell"))
             {
-                amount++;
+				if(Stores.StoreList[storename].StoreItemAmount > 0) {
+					Stores.StoreList[storename].StoreItemAmount++;
+				}
             }
 
             if (GUI.Button(new Rect(0, 80, 80, 20), "Close"))
---
+++ Assets/Scripts/Stores.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Stores {
+
+	public static Dictionary<string, StoreInventory> StoreList = new Dictionary<string, StoreInventory>();
+
+	public class StoreInventory {
+		public string StoreItem { get; set; }
+		public int StoreItemAmount { get; set; }
+	}
+}
